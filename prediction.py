6320266fc38624f126edb7650da4bc1b8e9fab5a
luctfyal12 <luctfyalkatiri08@gmail.com>
2025-10-08 20:02:17 +0700

    Initial Commit

--- prediction.py
+++ prediction.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
-import dill as pickle
+import pickle
 import numpy as np
 import preprocessing_utils
 
